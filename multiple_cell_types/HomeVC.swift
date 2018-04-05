1bb204753ef1c5a29b678c0d3e88c1311fa7f7e4
ruslanvs <ruslanvs@yahoo.com>
2018-04-05 15:35:32 -0700

    Initial Commit

---
+++ multiple_cell_types/HomeVC.swift
@@ -0,0 +1,24 @@
+//
+//  HomeVC.swift
+//  multiple_cell_types
+//
+//  Created by Ruslan Suvorov on 3/24/18.
+//  Copyright © 2018 Ruslan Suvorov. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
